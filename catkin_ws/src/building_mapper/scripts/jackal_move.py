b3a689d7b155ada1753737c3f8725ba3af1050d6
amkurup <amkurup@mtu.edu>
2017-02-26 00:57:47 -0500

    updating motion algorithm

--- catkin_ws/src/building_mapper/scripts/jackal_move.py
+++ catkin_ws/src/building_mapper/scripts/jackal_move.py
@@ -6,12 +6,12 @@
 # Intro to Robotics - EE5900 - Spring 2017
 #          Assignment #5
 
-#       Project #2 Group #2
+#       Project #5 Group #3
 #         Ian (Team Lead)
 #            Philip
 #            Akhil
 #
-# Revision: v1.2
+# Revision: v1.3
 
 # imports
 import rospy
@@ -23,11 +23,31 @@ import os
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+# init Twist msg
+motion = Twist()
+
+# motion algorithm in Callback routine for Jackal motion
 def Callback(data):
-     rospy.loginfo("Callback")
+    rospy.loginfo(data.ranges[-10])
+
+    # move forward
+    motion.linear.x  = 0.5
+    motion.angular.z = 0.0
+
+    # avoid left wall side
+    if data.ranges[-10] < 0.75 :
+        motion.linear.x  = 0.2
+        motion.angular.z = -0.2
+
+    # avoid right wall side
+    if data.ranges[10] < 0.75 :
+        motion.linear.x  = 0.2
+        motion.angular.z = 0.2
+
 
 # define setup and run routine
 def init():
+    global motion
     # create node for listening to twist messages
     rospy.init_node("building_mapper")
 
@@ -35,20 +55,17 @@ def init():
     rospy.Subscriber("/scan", LaserScan, Callback)
     rate = rospy.Rate(10)
 
-    # init Twist msg
-    motion = Twist()
     # publish to cmd_vel of the jackal
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
     while not rospy.is_shutdown():
 
-        # push Twist msgs
-        motion.linear.x = 0.5
-        motion.angular.z = 0
+        # push Twist msgs to override Callback algorithm
+        # motion.linear.x = 0.5
+        # motion.angular.z = 0
 
         # publish Twist
         pub.publish(motion)
-        # pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
         rate.sleep()
 
