f0f7d30631f82e75c55cc6fa613b80d5b8a8df29
amkurup <amkurup@mtu.edu>
2017-03-03 15:07:10 -0500

    updating discrete algorithm

--- catkin_ws/src/building_mapper/scripts/jackal_move.py
+++ catkin_ws/src/building_mapper/scripts/jackal_move.py
@@ -11,7 +11,7 @@
 #            Philip
 #            Akhil
 #
-# Revision: v1.4
+# Revision: v1.5
 
 # imports
 import rospy
@@ -71,64 +71,47 @@ class discrete_movement(object):
         self.motion.linear.x  = 0.5
         self.motion.angular.z = 0.0
 
-        # avoid right wall side
-        #if data.ranges[8] < 0.85 :
-        #    self.motion.linear.x  = 0.3
-        #    self.motion.angular.z = 0.25
-        #    rospy.loginfo('soft left turn')
+        avoid right wall side
+        if data.ranges[8] < 0.85 :
+           self.motion.linear.x  = 0.3
+           self.motion.angular.z = 0.25
+           rospy.logdebug('soft left turn')
 
-        # avoid left wall side
-        #if data.ranges[-8] < 0.85 :
-        #    self.motion.linear.x  = 0.3
-        #    self.motion.angular.z = -0.25
-        #    rospy.loginfo(' soft right turn')
+        avoid left wall side
+        if data.ranges[-8] < 0.85 :
+           self.motion.linear.x  = 0.3
+           self.motion.angular.z = -0.25
+           rospy.logdebug(' soft right turn')
 
-        # sharp turn if right side too close
-        #if data.ranges[8] < 0.4 :
-        #    self.motion.linear.x  = 0.15
-        #    self.motion.angular.z = 0.4
-        #    rospy.loginfo('hard left turn')
+        sharp turn if right side too close
+        if data.ranges[8] < 0.4 :
+           self.motion.linear.x  = 0.15
+           self.motion.angular.z = 0.4
+           rospy.logdebug('hard left turn')
 
-        # sharp turn if left side too close
-        #if data.ranges[-8] < 0.4 :
-        #    self.motion.linear.x  = 0.15
-        #    self.motion.angular.z = -0.4
-        #    rospy.loginfo('  hard right turn')
+        sharp turn if left side too close
+        if data.ranges[-8] < 0.4 :
+           self.motion.linear.x  = 0.15
+           self.motion.angular.z = -0.4
+           rospy.logdebug('  hard right turn')
 
-        # slow down if forward wall too close
-        #if data.ranges[90] < 1.5 :
-        #    self.motion.linear.x  = 0.3
-        #    self.motion.angular.z = 0.0
-        #    rospy.loginfo('slow down, wall ahead')
+        slow down if forward wall too close
+        if data.ranges[90] < 1.5 :
+           self.motion.linear.x  = 0.3
+           self.motion.angular.z = 0.0
+           rospy.logdebug('slow down, wall ahead')
 
-        # stop and turn at forward wall
-        #if data.ranges[90] < 1.2 :
-            # if right side obstacle is closer than left
-        #    if data.ranges[9] < data.ranges[-9]:
-        #        self.motion.linear.x  = 0.0
-        #        self.motion.angular.z = 0.2
-        #        rospy.loginfo('stop and turn left')
-        #    else:
-        #        self.motion.linear.x  = -0.15
-        #        self.motion.angular.z = -0.35
-        #        rospy.loginfo(' backup and turn right')
-
-        #rospy.loginfo(str(min(data.ranges)))
-
-        if min(data.ranges[:45]) < 0.7 :
-            self.motion.linear.x = 0.2
-            self.motion.angular.z = 0.2
-            rospy.loginfo('turn left')
-
-        if min(data.ranges[-45:]) < 0.7 :
-            self.motion.linear.x = 0.2
-            self.motion.angular.z = -0.2
-            rospy.loginfo('turn right')
-
-        if min(data.ranges[75:105]) < 0.6 :
-            self.motion.linear.x = -0.2
-            self.motion.angular.z = -0.3
-            rospy.loginfo('backup')
+        stop and turn at forward wall
+        if data.ranges[90] < 1.2 :
+            if right side obstacle is closer than left
+           if data.ranges[9] < data.ranges[-9]:
+               self.motion.linear.x  = 0.0
+               self.motion.angular.z = 0.2
+               rospy.logdebug('stop and turn left')
+           else:
+               self.motion.linear.x  = -0.15
+               self.motion.angular.z = -0.35
+               rospy.logdebug(' backup and turn right')
 
 # standard ros boilerplate
 if __name__ == "__main__":
