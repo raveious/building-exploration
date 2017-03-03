44577edf977880a16a99eafe34b2794ed3b80e7c
amkurup <amkurup@mtu.edu>
2017-03-03 13:45:06 -0500

    updating discrete algorithm

--- catkin_ws/src/building_mapper/scripts/jackal_move.py
+++ catkin_ws/src/building_mapper/scripts/jackal_move.py
@@ -65,51 +65,75 @@ class discrete_movement(object):
             rospy.signal_shutdown("Execution timer expired")
 
         # len(data.ranges) = 180
-        rospy.loginfo('left: %f  forward: %f  right: %f'%(data.ranges[-8], data.ranges[90], data.ranges[8]))
+        rospy.logdebug('left: %f  forward: %f  right: %f'%(data.ranges[-8], data.ranges[90], data.ranges[8]))
 
         # move forward
         self.motion.linear.x  = 0.5
         self.motion.angular.z = 0.0
 
         # avoid right wall side
-        if data.ranges[9] < 0.85 :
-            self.motion.linear.x  = 0.3
-            self.motion.angular.z = 0.25
+        #if data.ranges[8] < 0.85 :
+        #    self.motion.linear.x  = 0.3
+        #    self.motion.angular.z = 0.25
+        #    rospy.loginfo('soft left turn')
 
         # avoid left wall side
-        if data.ranges[-9] < 0.85 :
-            self.motion.linear.x  = 0.3
-            self.motion.angular.z = -0.25
+        #if data.ranges[-8] < 0.85 :
+        #    self.motion.linear.x  = 0.3
+        #    self.motion.angular.z = -0.25
+        #    rospy.loginfo(' soft right turn')
 
         # sharp turn if right side too close
-        if data.ranges[3] < 0.6 :
-            self.motion.linear.x  = 0.1
-            self.motion.angular.z = 0.3
+        #if data.ranges[8] < 0.4 :
+        #    self.motion.linear.x  = 0.15
+        #    self.motion.angular.z = 0.4
+        #    rospy.loginfo('hard left turn')
 
         # sharp turn if left side too close
-        if data.ranges[-3] < 0.6 :
-            self.motion.linear.x  = 0.1
-            self.motion.angular.z = -0.3
+        #if data.ranges[-8] < 0.4 :
+        #    self.motion.linear.x  = 0.15
+        #    self.motion.angular.z = -0.4
+        #    rospy.loginfo('  hard right turn')
 
         # slow down if forward wall too close
-        if data.ranges[90] < 1.5 :
-            self.motion.linear.x  = 0.3
-            self.motion.angular.z = 0.0
+        #if data.ranges[90] < 1.5 :
+        #    self.motion.linear.x  = 0.3
+        #    self.motion.angular.z = 0.0
+        #    rospy.loginfo('slow down, wall ahead')
 
         # stop and turn at forward wall
-        if data.ranges[90] < 1.2 :
-            if data.ranges[9]<data.ranges[-9]:
-                self.motion.linear.x  = 0.0
-                self.motion.angular.z = 0.2
-            else:
-                self.motion.linear.x  = -0.1
-                self.motion.angular.z = -0.3
+        #if data.ranges[90] < 1.2 :
+            # if right side obstacle is closer than left
+        #    if data.ranges[9] < data.ranges[-9]:
+        #        self.motion.linear.x  = 0.0
+        #        self.motion.angular.z = 0.2
+        #        rospy.loginfo('stop and turn left')
+        #    else:
+        #        self.motion.linear.x  = -0.15
+        #        self.motion.angular.z = -0.35
+        #        rospy.loginfo(' backup and turn right')
 
+        #rospy.loginfo(str(min(data.ranges)))
+
+        if min(data.ranges[:45]) < 0.7 :
+            self.motion.linear.x = 0.2
+            self.motion.angular.z = 0.2
+            rospy.loginfo('turn left')
+
+        if min(data.ranges[-45:]) < 0.7 :
+            self.motion.linear.x = 0.2
+            self.motion.angular.z = -0.2
+            rospy.loginfo('turn right')
+      
+        if min(data.ranges[75:105]) < 0.6 :
+            self.motion.linear.x = -0.2
+            self.motion.angular.z = -0.3
+            rospy.loginfo('backup')
 
 # standard ros boilerplate
 if __name__ == "__main__":
     try:
-        run = discrete_movement(300) #timeout seconds
+        run = discrete_movement(30) #timeout seconds
     except rospy.ROSInterruptException:
         pass
 
